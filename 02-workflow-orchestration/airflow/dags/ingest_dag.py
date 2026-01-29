fcf0cb8cc7b25f65faa8d181fb5ba6d3b5bc5cf6
Saurabh Jain <saurabhjainjs@gmail.com>
2026-01-29 22:26:04 +0000

    Updated the DAG template to run the ingestion

--- 02-workflow-orchestration/airflow/dags/ingest_dag.py
+++ 02-workflow-orchestration/airflow/dags/ingest_dag.py
@@ -13,19 +13,22 @@ default_args = {
 
 # 2. Define the DAG
 dag = DAG(
-    'taxi_ingest_v01',
+    'taxi_ingest_v02',
     default_args=default_args,
     description='Testing our Docker ingestion worker',
     schedule_interval='@monthly',       # Only run when we click "Play"
-    start_date=datetime(2021, 1, 1),    # A fixed start date
-    catchup=False                       # DO NOT run historical dates yet
+    start_date=datetime(2020, 1, 1),    # A fixed start date
+    end_date=datetime(2020, 3, 1),      # A fixed end date
+    catchup=True,                       # DO NOT run historical dates yet if it is set to False
+    max_active_runs=2,                  # Only two active run at a time                   
 )
 
 # 3. The Task
 ingest_task = DockerOperator(
-    task_id='ingest_january_2021',
+    task_id='ingest_taxi_data',
     image='taxi_ingest:v001',
-    container_name='ingest_worker_jan_2021',
+    # Make the container name unique per run!
+    container_name='ingest_worker_{{ ds_nodash }}',
     api_version='auto',
     auto_remove=True,
     network_mode='airflow_default',
