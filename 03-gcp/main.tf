21c4bdac215d212fc49b2e93d330aa96da026707
Saurabh Jain <saurabhjainjs@gmail.com>
2026-02-01 04:13:32 +0000

    Added resources to main.tf file

--- 03-gcp/main.tf
+++ 03-gcp/main.tf
@@ -9,6 +9,28 @@ terraform {
 
 provider "google" {
   # Replace with your actual GCP Project ID
-  project = "nyc-taxi-data-pipeline-485822" 
+  project = "nyc-taxi-data-pipeline-485822"
   region  = "us-east1"
+}
+
+# Create a GCS Bucket (Data Lake)
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "nyc-taxi-data-pipeline-485822-bucket" # Must be globally unique
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+# Create a BigQuery Dataset (Data Warehouse)
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = "nyc_taxi_dataset"
+  location   = "US"
 }
