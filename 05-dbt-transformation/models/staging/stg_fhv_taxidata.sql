545b9a17768cef7dca106049d72885fb06b66357
Saurabh Jain <saurabhjainjs@gmail.com>
2026-02-13 21:36:07 +0000

    updated

--- 05-dbt-transformation/models/staging/stg_fhv_taxidata.sql
+++ 05-dbt-transformation/models/staging/stg_fhv_taxidata.sql
@@ -5,7 +5,7 @@ from {{ source('staging', 'fhv_taxi_data_2019') }}
 where dispatching_base_num is not null
 )
 
-select {{ dbt_utils.generate_surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+select {{ dbt_utils.generate_surrogate_key(['dispatching_base_num', 'pickup_datetime', 'dropoff_datetime']) }} as tripid,
        cast(dispatching_base_num as string) as dispatching_base_num,
        cast(pickup_datetime as timestamp) as pickup_datetime,
        cast(dropoff_datetime as timestamp) as dropoff_datetime,
