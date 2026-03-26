c6b702fe4e94f141db60f8c8d0872c23f185700d
Saurabh Jain <saurabhjainjs@gmail.com>
2026-03-26 00:00:01 +0000

    Adding jupyter notebooks

---
+++ 07-streaming/notebooks/models.py
@@ -0,0 +1,35 @@
+# 1. Define the Blueprint
+from dataclasses import dataclass
+import dataclasses
+import json
+
+@dataclass
+class Ride:
+    PULocationID: int
+    DOLocationID: int
+    trip_distance: float
+    total_amount: float
+    tpep_pickup_datetime: int  # epoch milliseconds
+
+
+def ride_from_row(row):
+    return Ride(
+        PULocationID=int(row['PULocationID']),
+        DOLocationID=int(row['DOLocationID']),
+        trip_distance=float(row['trip_distance']),
+        total_amount=float(row['total_amount']),
+        tpep_pickup_datetime=int(row['tpep_pickup_datetime'].timestamp() * 1000),
+    )
+
+
+
+def ride_serializer(ride):
+    ride_dict = dataclasses.asdict(ride)
+    json_str = json.dumps(ride_dict)
+    return json_str.encode('utf-8')
+
+
+def ride_deserializer(data):
+    json_str = data.decode('utf-8')
+    ride_dict = json.loads(json_str)
+    return Ride(**ride_dict)
